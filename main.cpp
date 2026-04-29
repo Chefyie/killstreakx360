f3ea1962b2bc12295f9dd081264ac374ce37163c
Chefy <94152428+Chefyie@users.noreply.github.com>
2026-04-28 21:06:10 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 /*
 This code is currently empty, not released.
 You could either wait for Cosmos Public Plugin to release.
+You would have to purchase at XBLK to get it (to avoid bad actors)
 Thanks!
 */
